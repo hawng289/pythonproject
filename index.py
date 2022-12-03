d257fff9d32595bbf5171c13f703c55791dff40f
hanging <114472721+hawng289@users.noreply.github.com>
2022-12-03 18:20:29 +0700

    Update index.py

--- index.py
+++ index.py
@@ -1,45 +1,25 @@
-import requests
+from pyodide.http import pyfetch
+import asyncio
 from bs4 import BeautifulSoup
 
-def get_testdata(path) :
-    try:
-        res = requests.get(path)
-        res.raise_for_status()
-    except requests.exceptions.URLRequired as erru:
-        return erru
-    except requests.exceptions.TooManyRedirects as e:
-        return e
-    except requests.exceptions.HTTPError as errh:
-        return errh
-    except requests.exceptions.ConnectionError as errc:
-        return errc
-    except requests.exceptions.Timeout as errt:
-        return errt
-    except requests.exceptions.RequestException as err:
-        return err
-   
-    else:
-        obj = BeautifulSoup(res.text, 'html.parser')
-        list_title = []
-        for i in range(1, 7):
-            h = obj.find_all('h' + str(i))
-            if (h == []):
-                break
-            list_title.append(h) 
-        content = ""
-        for item in list_title:
-            for i in item:
-                title_text = i.get_text()
-                content += title_text
-        list_text = obj.find_all('p')
-        for item in list_text:
-            text = item.get_text()
-            if (len(text) > 50):
-                content += text
-        return content
-print(get_testdata("https://www.andreasjakl.com/raycast-anchor-placing-ar-foundation-holograms-part-3/"))
-
-
-
-
-
+async def get_testdata(path) :
+    path = "https://www.andreasjakl.com/raycast-anchor-placing-ar-foundation-holograms-part-3/"
+    res = await pyfetch(url=path, method="GET")
+    obj = BeautifulSoup(await res.string(), 'html.parser')
+    list_title = []
+    for i in range(1, 7):
+        h = obj.find_all('h' + str(i))
+        if (h == []):
+            break
+        list_title.append(h) 
+    content = ""
+    for item in list_title:
+        for i in item:
+            title_text = i.get_text()
+            content += title_text
+    list_text = obj.find_all('p')
+    for item in list_text:
+        text = item.get_text()
+        if (len(text) > 50):
+            content += text
+    return content
